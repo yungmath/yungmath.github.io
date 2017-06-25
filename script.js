dea47b4a2a6cb0c7b5d4578ffeea3b06d1d46792
wongbryan <wongbryanwongbryan@gmail.com>
2017-06-25 01:26:48 -0700

    checkout page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const VIDEO = document.getElementsByTagName('video')[0];
+const GALLERY = document.getElementsByClassName('gallery')[0];
 var WIDTH = window.innerWidth; 
 var HEIGHT = window.innerHeight;
 
@@ -10,5 +11,26 @@ else
 VIDEO.addEventListener('ended', function(){
 	var main = document.getElementsByClassName('main')[0];
 	main.classList.add('transparent');
-	
-})
+	GALLERY.classList.remove('none');
+	GALLERY.classList.remove('transparent');
+	GALLERY.classList.add('flex');
+	setTimeout(function(){
+		document.body.removeChild(main);
+	}, 1500);
+});
+
+var bar = document.getElementsByClassName('navBar')[0];
+function checkNavBar(){
+	var scrollTop = Number(document.body.scrollTop);
+	var scrollTotal = Number(document.body.scrollHeight)-Number(window.innerHeight);
+	if(scrollTop < 50){
+		bar.classList.add('hidden');
+		bar.classList.remove('show');
+	}
+	else{
+		bar.classList.add('show');
+		bar.classList.remove('hidden');
+	}
+}
+
+window.addEventListener('scroll', checkNavBar);
